1458d00c8821e63ead03688aeef58341125a7ad7
Akshit Goel <akshit.goel.03@gmail.com>
2026-02-08 20:11:41 +0530

    Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
 import './App.css'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Donate from './pages/Donate'
+import GetInvolved from './pages/GetInvolved'
+import Media from './pages/Media'
+import NotFound from './pages/NotFound'
+import Testimonials from './pages/Testimonials'
+
+
 
 function App() {
   
   return (
     <>
-      <div>
-        
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/get-involved" element={<GetInvolved />} />
+          <Route path="/media" element={<Media />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
